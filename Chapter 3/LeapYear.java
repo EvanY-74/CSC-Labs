87832f70a884e86b46426ccec2f755a1bdefcfe9
EvanY-74 <138799210+EvanY-74@users.noreply.github.com>
2025-09-09 09:08:12 -0400

    changed number to year in LeapYear

--- Chapter 3/LeapYear.java
+++ Chapter 3/LeapYear.java
@@ -9,7 +9,7 @@ public class LeapYear {
     }
     
     public static void main(String[] args) {
-        int year = getIntInput("Enter a number: ");
+        int year = getIntInput("Enter a year: ");
         if (year % 400 == 0) System.out.println(year + " is a leap year.");
         else if (year % 100 == 0) System.out.println(year + " is not a leap year.");
         else if (year % 4 == 0) System.out.println(year + " is a leap year.");
